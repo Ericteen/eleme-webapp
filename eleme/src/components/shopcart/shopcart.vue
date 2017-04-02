b62d69afd1b05331ed761fc242fd36da15e71619
Ericteen <chengteen@yeah.net>
2017-04-02 16:10:24 +0800

    add new component

---
+++ eleme/src/components/shopcart/shopcart.vue
@@ -0,0 +1,38 @@
+<template>
+	<div class="shopcart">
+		<div class="content">
+			<div class="content-left">
+				<div class="logo-wrapper">
+					<div class="logo">
+						<i class="icon-shopping_cart"></i>
+					</div>
+				</div>
+				<div class="price"></div>
+				<div class="desc"></div>
+			</div>
+			<div class="content-right"></div>
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {}
+</script>
+
+<style lang='stylus'>
+	.shopcart
+		position: fixed
+		left: 0
+		bottom: 0
+		z-index: 50
+		width: 100%
+		height: 48px
+		.content
+			display: flex
+			background: #141d27
+			.content-left
+				flex: 1
+			.content-right
+				flex: 0 0 105px
+				width: 105px
+</style>
