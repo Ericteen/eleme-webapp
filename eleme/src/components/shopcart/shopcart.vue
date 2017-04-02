ac44d88cb8bd1ef18fc9f3e86229fb941264fea0
Ericteen <chengteen@yeah.net>
2017-04-02 20:29:00 +0800

    shopping cart component

--- eleme/src/components/shopcart/shopcart.vue
+++ eleme/src/components/shopcart/shopcart.vue
@@ -7,8 +7,8 @@
 						<i class="icon-shopping_cart"></i>
 					</div>
 				</div>
-				<div class="price"></div>
-				<div class="desc"></div>
+				<div class="price">￥0</div>
+				<div class="desc">另需配送费￥{{ deliveryPrice }}元</div>
 			</div>
 			<div class="content-right"></div>
 		</div>
@@ -16,7 +16,18 @@
 </template>
 
 <script>
-	export default {}
+	export default {
+		props: {
+			deliveryPrice: {
+				type: Number,
+				default: 0
+			},
+			minPrice: {
+				type: Number,
+				default: 0
+			}
+		}
+	}
 </script>
 
 <style lang='stylus'>
@@ -30,8 +41,50 @@
 		.content
 			display: flex
 			background: #141d27
+			font-size: 0
 			.content-left
 				flex: 1
+				.logo-wrapper
+					display: inline-block
+					vertical-align: top
+					position: relative
+					top: -10px
+					margin: 0 12px
+					padding: 6px
+					width: 56px
+					height: 56px
+					box-sizing: border-box
+					
+					border-radius: 50%
+					background: #141d27
+					.logo
+						width: 100%
+						height: 100%
+						border-radius: 50%
+						text-align: center
+						background: #2b343c
+						.icon-shopping_cart
+							line-height: 44px
+							font-size: 24px
+							color: #80858a
+				.price
+					display: inline-block
+					vertical-align: top
+					margin-top: 12px
+					line-height: 24px
+					padding-right: 12px
+					box-sizing: border-box
+					border-right: 1px solid rgba(255, 255, 255, 0.1)
+					font-size: 16px
+					font-weight: 700
+					color: rgba(255, 255, 255, 0.4) 
+				.desc
+					display: inline-block
+					vertical-align: top
+					line-height: 24px
+					margin: 12px 0 0 12px
+					color: rgba(255, 255, 255, 0.4)
+					font-size: 10px
 			.content-right
 				flex: 0 0 105px
 				width: 105px
