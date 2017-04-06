ea469dc625c7c1c343ddbe8ffff2d29385505b64
Ericteen <chengteen@yeah.net>
2017-04-07 00:19:02 +0800

    shopcart addded

--- eleme/src/components/shopcart/shopcart.vue
+++ eleme/src/components/shopcart/shopcart.vue
@@ -3,43 +3,14 @@
 		<div class="content">
 			<div class="content-left">
 				<div class="logo-wrapper">
-					<div class="logo" :class="{'highlight':totalCount>0}">
-						<i class="icon-shopping_cart" :class="{'highlight':totalCount>0}"></i>
+					<div class="logo">
+						<i class="icon-shopping_cart"></i>
 					</div>
-					<div class="num" v-show='totalCount>0'>{{ totalCount }}</div>
 				</div>
-				<div class="price" :class="{'highlight':totalPrice>0}">￥{{ totalPrice }}</div>
+				<div class="price">￥0</div>
 				<div class="desc">另需配送费￥{{ deliveryPrice }}元</div>
 			</div>
-			<div class="content-right">
-				<div class="pay" :class='payClass'>
-					{{ payDesc }}
-				</div>
-			</div>
-			<div class="ball-container">
-				<div v-for='ball in balls'>
-					<transition name='drop' @before-enter='beforeDrop' @enter='dropping' @after-drop='afterDrop'>
-						<div class="ball" v-show='ball.show'>
-							<div class="inner inner-hook"></div>
-						</div>
-					</transition>
-				</div>
-			</div>
-			<div class="shopcart-list" v-show='listShow'>
-				<div class="list-header">
-					<h1 class="title">购物车</h1>
-					<span class="empty">清空</span>
-				</div>
-				<div class="list-content">
-					<ul>
-						<li class="food" v-for='food in selectFoods'></li>
-						<span class="name">{{ food.name }}</span>
-						<div class="price">
-							<span>￥{{ food.price * food.count }}</span>
-						</div>
-					</ul>
-				</div>
-			</div>
+			<div class="content-right"></div>
 		</div>
 	</div>
 </template>
@@ -47,12 +18,6 @@
 <script>
 	export default {
 		props: {
-			selectFoods: {
-				type: Array,
-				default () {
-					return []
-				}
-			},
 			deliveryPrice: {
 				type: Number,
 				default: 0
@@ -60,60 +25,6 @@
 			minPrice: {
 				type: Number,
 				default: 0
-			}
-		},
-		data () {
-			return {
-				balls: [
-					{
-						show: false
-					},
-					{
-						show: false
-					},
-					{
-						show: false
-					},
-					{
-						show: false
-					},
-					{
-						show: false
-					}
-				]
-			}
-		},
-		computed: {
-			totalPrice () {
-				let total = 0
-				this.selectFoods.forEach((food) => {
-					total += food.price * food.count
-				})
-				return total
-			},
-			totalCount () {
-				let count = 0
-				this.selectFoods.forEach((food) => {
-					count += food.count
-				})
-				return count
-			},
-			payDesc () {
-				if (this.totalPrice === 0) {
-					return `${this.minPrice}元起送`
-				} else if (this.totalPrice < this.minPrice) {
-					let diff = this.minPrice - this.totalPrice
-					return `还差${diff}元起送`
-				} else {
-					return '去结算'
-				}
-			},
-			payClass () {
-				if (this.totalPrice < this.minPrice) {
-					return 'not-enough'
-				} else {
-					return 'enough'
-				}
 			}
 		}
 	}
@@ -131,7 +42,6 @@
 			display: flex
 			background: #141d27
 			font-size: 0
-			color: rgba(255, 255, 255, 0.4)
 			.content-left
 				flex: 1
 				.logo-wrapper
@@ -143,7 +53,8 @@
 					padding: 6px
 					width: 56px
 					height: 56px
-					box-sizing: border-box					
+					box-sizing: border-box
+					
 					border-radius: 50%
 					background: #141d27
 					.logo
@@ -152,28 +63,10 @@
 						border-radius: 50%
 						text-align: center
 						background: #2b343c
-						&.highlight
-							background: rgb(0, 160, 220)
 						.icon-shopping_cart
 							line-height: 44px
 							font-size: 24px
 							color: #80858a
-							&.highlight
-								color: #fff
-					.num
-						position: absolute
-						top: 0
-						right: 0
-						width: 24px
-						height: 16px
-						line-height: 16px
-						text-align: center
-						border-radius: 16px
-						font-size: 9px
-						font-weight: 700
-						color: #fff
-						background: rgb(240, 20, 20)
-						box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.4)
 				.price
 					display: inline-block
 					vertical-align: top
@@ -184,30 +77,15 @@
 					border-right: 1px solid rgba(255, 255, 255, 0.1)
 					font-size: 16px
 					font-weight: 700
-					&.highlight
-						color: #fff
+					color: rgba(255, 255, 255, 0.4) 
 				.desc
 					display: inline-block
 					vertical-align: top
 					line-height: 24px
 					margin: 12px 0 0 12px
+					color: rgba(255, 255, 255, 0.4)
 					font-size: 10px
 			.content-right
 				flex: 0 0 105px
 				width: 105px
-				.pay
-					height: 48px
-					line-height: 48px
-					text-align: center
-					font-size: 12px
-					font-weight: 700
-					&.not-enough
-						background: #2b333b
-					&.enough
-						background: #00b43c
-						color: #fff
-		.ball-container
-			.ball
-				position: fixed
-
 </style>
