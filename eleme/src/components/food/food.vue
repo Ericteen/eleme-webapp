157b9d8081770eab70e139a76e6136bb42f0e9af
Ericteen <chengteen@yeah.net>
2017-04-10 11:13:54 +0800

    product detail page

--- eleme/src/components/food/food.vue
+++ eleme/src/components/food/food.vue
@@ -1,6 +1,6 @@
 <template>
 	<transition name='move'>
-		<div v-show='showFlag' class='food'>
+		<div v-show='showFlag' class='food' ref='food'>
 			<div class="food-content">
 				<div class="image-header">
 					<img :src="food.image" alt="">
@@ -17,13 +17,24 @@
 					<div class="price">
 						<span class="now">￥{{ food.price }}</span><span class="old" v-show='food.oldPrice'>￥{{ food.oldPrice }}</span>
 					</div>
-				</div>
+					<div class="cartcontrol-wrapper">
+						<cartcontrol @add='addFood' :food='food'></cartcontrol>
+					</div>
+					<transition class="fade">
+						<div @click.stop.prevent='addFirst' class="buy" v-show='!food.count || food.count===0'>加入购物车</div>
+					</transition>
+					
+				</div>	
 			</div>
 		</div>
 	</transition>
 </template>
 
 <script>
+import Vue from 'vue'
+import BScroll from 'better-scroll'
+import Cartcontrol from '@/components/cartcontrol/cartcontrol'
+
 	export default {
 		props: {
 			food: {
@@ -38,10 +49,32 @@
 		methods: {
 			show () {
 				this.showFlag = true
+				this.$nextTick(() => {
+					if (!this.scroll) {
+						this.scroll = new BScroll(this.$refs.food, {
+							click: true
+						})
+					} else {
+						this.scroll.refresh()
+					}
+				})
 			},
 			hide () {
 				this.showFlag = false
+			},
+			addFirst (event) {
+				if (!event._constructed) {
+					return
+				}
+				this.$emit('add', event.target)
+				Vue.set(this.food, 'count', 1)
+			},
+			addFood (target) {
+				this.$emit('add', target)
 			}
+		},
+		components: {
+			Cartcontrol
 		}
 	}
 </script>
@@ -111,4 +144,27 @@
 					text-decoration: line-through
 					font-size: 10px
 					color: rgb(147, 153, 159)
+			.cartcontrol-wrapper
+				position: absolute
+				right: 12px
+				bottom: 12px
+			.buy
+				position: absolute
+				right: 18px
+				bottom: 18px
+				z-index: 10
+				height: 24px
+				line-height: 24px
+				padding: 0 12px
+				box-sizing: border-box
+				border-radius: 12px
+				font-size: 10px
+				color: #fff
+				background: rgb(0, 160, 220)
+				opacity: 1
+				&.fade-enter-active, &.fade-leave-active
+					transition: all 0.2s
+				&.fade-enter, &.fade-leave-active
+					opacity: 0
+					z-index: -1
 </style>
